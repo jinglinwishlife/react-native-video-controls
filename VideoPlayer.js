231f1ee91109d57c19a26627d1f70b90270f11e1
jinglin_wishlife <jing.lin@wishlife.com>
2017-12-01 18:16:55 -0800

    minor fix

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -824,8 +824,8 @@ export default class VideoPlayer extends Component {
                         { backControl }
                         <View style={ styles.controls.pullRight }>
                             { volumeControl }
-                            { muteVolume }
                             { fullscreenControl }
+                            { muteVolume }
                         </View>
                     </View>
                 </ImageBackground>
